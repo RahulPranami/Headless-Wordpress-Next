d073bace747f9c802bec395a78db89503ffc7db9
RahulPranami <rahulpranami101@gmail.com>
2023-10-02 13:37:54 +0530

    added Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter, Cookie } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/Header";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 const cookie = Cookie({ subsets: ["latin"], weight: "400" });
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           <Header />
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
